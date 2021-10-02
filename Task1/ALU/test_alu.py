e1e28ab32882c1acb19432ace398615798e0df70
ALI11-2000 <59641896+ALI11-2000@users.noreply.github.com>
2021-10-02 18:12:12 +0500

    Update test_alu.py

--- Task1/ALU/test_alu.py
+++ Task1/ALU/test_alu.py
@@ -14,16 +14,20 @@ async def alu_test(dut):
         A = random.randint(0,10)
         B = random.randint(0,10)
         sel = random.randint(0,7)
+        
         dut.A.value = A
         dut.B.value = B
         dut.sel.value = sel
+        
         #awaiting 2ns
         await Timer(2,units='ns')
         output1=alu_model(A,B,sel)
+        
         #showing the output log info
         dut._log.info( "Randomised test with: %d  {y}  %d = %d for model \
-            value {a}".format(y=x[sel],a=output1),dut.A.value,dut.B.value,dut.X.value)
+        value {a}".format(y=x[sel],a=output1),dut.A.value,dut.B.value,dut.X.value)
+        
         #asserting error if the test fails
         assert dut.X.value == output1, "Randomised test failed with: {A} \
-             {y}  {B} = {X} for {a}".format(A=dut.A.value, B=dut.B.value, \
-                  X=dut.X.value,y=x[sel],a=output1)
+        {y}  {B} = {X} for {a}".format(A=dut.A.value, B=dut.B.value,     \
+        X=dut.X.value,y=x[sel],a=output1)
