0532b01f21d8a21f0b682d5823d99dbd652ad35e
Faizan Zulfiqar <faizanzulfi@gmail.com>
2021-10-25 13:06:13 +0500

    Adding IP Verification

---
+++ IP Verification/New Test/test_alu.py
@@ -0,0 +1,41 @@
+#importing required libraries
+import cocotb
+from cocotb import clock
+from cocotb.triggers import RisingEdge, Timer
+from cocotb.clock import Clock
+from cocotb.handle import BinaryRepresentation
+import random
+import logging
+
+from codecs import decode
+import struct
+
+
+def bin_to_float(b):
+    """ Convert binary string to a float. """
+    bf = int_to_bytes(int(b, 2), 8)  # 8 bytes needed for IEEE 754 binary64.
+    return struct.unpack('>d', bf)[0]
+
+
+def int_to_bytes(n, length):  # Helper function
+    """ Int/long to byte string.
+
+        Python 3.2+ has a built-in int.to_bytes() method that could be used
+        instead, but the following works in earlier versions including 2.x.
+    """
+    return decode('%%0%dx' % (length << 1) % n, 'hex')[-length:]
+
+#initiating cocotb test
+@cocotb.test()
+async def alu_test(dut):
+    clk = clock.Clock(dut.clk,10,'ns')
+    cocotb.fork(clk.start())
+    dut.rst.value = 1
+    dut.degrees.value = 90
+    dut.actv.value = 0
+    dut.enable.value = 1
+    for i in range(1):await RisingEdge(dut.clk)
+    dut.rst.value = 0
+    for i in range(50): await RisingEdge(dut.clk)
+    result = bin_to_float(str(dut.data1.value))
+    print("input is ",(dut.degrees),"and sine output is ",(result))
